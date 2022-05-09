ca86e08ece37ce840c67c174391d3a720501dd70
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-05-09 14:33:03 -0600

    Finish styles of the hamburguer menu

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector('#nav-bar-btn');
 const navMenu = document.querySelector('.mobile-menu');
 const mainLogo = document.querySelector('#main-logo');
+const headerB = document.querySelector('#mobile-header');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   mainLogo.classList.toggle('is-hidden');
+  headerB.classList.toggle('header-b');
 });
