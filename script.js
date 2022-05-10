66ab432f3e5a6f319f27e9f36fad060f20797dab
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-05-10 12:05:29 -0600

    Finish actions on the links for the hamburguer menu in js

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ const navMenu = document.querySelector('.mobile-menu');
 const mainLogo = document.querySelector('#main-logo');
 const headerB = document.querySelector('#mobile-header');
 
+/*
+Open Hamburguer menu when the icon is clicked
+*/
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   mainLogo.classList.toggle('is-hidden');
   headerB.classList.toggle('header-b');
 });
+
+/*
+Close Menu when you click the nav links
+*/
+document.querySelectorAll('.mobile-link').forEach(n => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+  mainLogo.classList.remove('is-hidden');
+  headerB.classList.remove('header-b');
+}));
